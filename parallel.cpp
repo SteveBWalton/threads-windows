5859057d188a01b1bc2fe3f5161529ae75c7ea45
Steve Walton <37951256+SteveBWalton@users.noreply.github.com>
2022-08-11 15:31:11 +0100

    Added actual work to the test function.

--- parallel.cpp
+++ parallel.cpp
@@ -5,35 +5,55 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
+#include <functional>
+#include <cmath>
 
 // Application Headers.
 #include "thread_pool.h"
 
 
 
+// Write the number to the console and pause for a little while.
 void showNumber
 (
     int number
 )
 {
+    // Write the number to console.
     std::cout << number << " ";
     std::cout.flush();
 
-    
-    // Not available in Windows.    
-    // usleep(1000000);
+    // True to sleep afterward, false for actual calculations.
+    const bool SLEEP = false;
 
-    // Sleep for 1 second.
-    std::this_thread::sleep_for(std::chrono::microseconds(1000000));
+    // Pause for a little while.
+    if (SLEEP)
+    {
+        // Sleep for 1 second.
+        std::this_thread::sleep_for(std::chrono::microseconds(1000000));
+    }
+    else
+    {
+        // Do actual work that hurts the processor.
+        for (int i = 0; i < 10000000; i++)
+        {
+            double result = sqrt((double)i);
+            result = pow(result, 0.78);
+            result = pow(result, 1.32);
+            result = pow(result, 0.57);
+        }
+    }
 }
 
 
 
+// Compare parallel loops and serial loops.
 int main()
 {
+    // The number loops for the test.
     const int NUM_LOOPS = 30;
 
-    std::cout << "Hello World!\n";
+    std::cout << "Hello from Parallel.\n";
 
     // Call the function n times in serial.
     for (int i = 0; i < NUM_LOOPS; i++)
